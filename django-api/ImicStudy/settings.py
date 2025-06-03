598daec49aed3e72d39e77bba011a7d8e9d204c0
mk-pku <motowaki.kakeru@pkutech.co.jp>
2025-06-03 09:24:28 +0900

    家計簿Tableの作成完了

--- django-api/ImicStudy/settings.py
+++ django-api/ImicStudy/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'ImicMainApp.apps.ImicmainappConfig',
+    'kakeibo.apps.KakeiboConfig',
 ]
 
 MIDDLEWARE = [
