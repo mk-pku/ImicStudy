812ba8c56e9c73e6d4069475e318f963d034addc
mk-pku <motowaki.kakeru@pkutech.co.jp>
2025-06-03 06:28:50 +0000

    途中経過

--- django-api/ImicStudy/urls.py
+++ django-api/ImicStudy/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('kakeibo.urls')),
+    path('api/', include('kakeibo.urls')),
 ]
--- django-api/kakeibo/urls.py
+++ django-api/kakeibo/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('api/transaction/<int:transaction_id>/', views.get_transaction_by_id_sqlalchemy, name='api_get_transaction_sqlalchemy'),
+    path('transaction/<int:transaction_id>/', views.retrieve_transactions, name='retrieve_transactions'),
 ]
