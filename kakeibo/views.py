71c142f99b04cdd8252b099d6942a1a2029ed1cf
mk-pku <motowaki.kakeru@pkutech.co.jp>
2025-06-03 06:28:50 +0000

    途中経過

--- kakeibo/views.py
+++ kakeibo/views.py
@@ -1,19 +1,60 @@
-from django.http import JsonResponse, Http404
-from .sqlalchemy import SessionLocal
+from django.http import JsonResponse, Http404, HttpResponseBadRequest
+from django.views.decorators.http import require_http_methods
+from .sqlalchemy import get_session
 from .sqlalchemy import TransactionSQL
 
 
-def get_transaction_by_id_sqlalchemy(request, transaction_id):
-    db_session = SessionLocal()
+@require_http_methods(["GET"])
+def list_transactions(request):
+    """
+    TODO: 取引一覧を取得する処理
+    """
+    session = get_session()
     try:
-        transaction_record = db_session.query(TransactionSQL).filter(TransactionSQL.id == transaction_id).first()
+        return None
+    finally:
+        session.close()
 
+@require_http_methods(["GET"])
+def retrieve_transactions(request, transaction_id):
+    session = get_session()
+    try:
+        transaction_record = session.get(TransactionSQL, transaction_id)
         if transaction_record is None:
             raise Http404("指定されたIDの家計簿レコードは見つかりません。")
-        
         return JsonResponse(transaction_record.to_dict())
-    
-    except Exception as e:
-        return JsonResponse({'error': str(e)}, status=500)
     finally:
-        db_session.close()
+        session.close()
+
+@require_http_methods(["POST"])
+def create_transaction(request):
+    """
+    TODO: 新規取引を作成する処理
+    """
+    session = get_session()
+    try:
+        return None
+    finally:
+        session.close()
+
+@require_http_methods(["PUT", "PATCH"])
+def update_transaction(request, transaction_id):
+    """
+    TODO: 指定IDの取引を更新する処理
+    """
+    session = get_session()
+    try:
+        return None
+    finally:
+        session.close()
+
+@require_http_methods(["DELETE"])
+def delete_transaction(request, transaction_id):
+    """
+    TODO: 指定IDの取引を削除する処理を
+    """
+    session = get_session()
+    try:
+        return None
+    finally:
+        session.close()
