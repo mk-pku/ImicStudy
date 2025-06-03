ac6066a48be494c88c1b064500596261ba77c866
mk-pku <motowaki.kakeru@pkutech.co.jp>
2025-06-03 11:53:27 +0900

    api動作確認完了

--- kakeibo/models.py
+++ kakeibo/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Category(models.Model):
 	id = models.IntegerField("ID", primary_key=True)
 	name = models.CharField("名前", max_length=20)
@@ -13,12 +14,15 @@ class Category(models.Model):
 	def __str__(self):
 		return self.name
 
+
 class Transaction(models.Model):
 	date = models.DateField("日付")
 	category = models.ForeignKey(Category, on_delete=models.PROTECT, verbose_name="費目")
 	memo = models.CharField("メモ", max_length=100, null=True, blank=True)
 	income = models.IntegerField("入金額", default=0)
 	expenditure = models.IntegerField("出金額", default=0)
+	created_at = models.DateTimeField("作成日時", auto_now_add=True)
+	updated_at = models.DateTimeField("更新日時", auto_now=True)
 	
 	class Meta:
 		db_table = '家計簿'
