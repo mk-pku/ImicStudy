717b0ba0672dd6faaa526167706faa129c269f88
mk-pku <motowaki.kakeru@pkutech.co.jp>
2025-06-04 05:10:05 +0000

    apiの実装のみ完了

--- kakeibo/tests/views/test_create_transaction.py
+++ kakeibo/tests/views/test_create_transaction.py
@@ -1,37 +1,8 @@
 import json
-from django.test import TestCase, Client
-from ..sqlalchemy import Base, get_session, get_engine, CategorySQL, TransactionSQL
+from ..base import BaseTestCase
 
 
-class TransactionAPITests(TestCase):
-	@classmethod
-	def setUpClass(cls):
-		super().setUpClass()
-		engine = get_engine()
-		Base.metadata.create_all(bind=engine)
-
-		session = get_session()
-		try:
-			categories = [
-				CategorySQL(id=1, name="給料", notes="給与等の定期収入"),
-				CategorySQL(id=18, name="趣味・娯楽", notes="映画やゲーム等"),
-			]
-			session.add_all(categories)
-			session.commit()
-		finally:
-			session.close()
-
-	def setUp(self):
-		self.client = Client()
-	
-	def tearDown(self):
-		session = get_session()
-		try:
-			session.query(TransactionSQL).delete()
-			session.commit()
-		finally:
-			session.close()
-	
+class CreateTransactionTests(BaseTestCase):
 	def test_create_transaction_success(self):
 		payload = {
 			"date": "2025-06-01",
